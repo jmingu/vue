5b8781c23636d76a8f34677d0985d810f8713234
JMG <jmg173@naver.com>
2024-05-18 01:29:26 +0900

    뷰 투두프로젝트 시작

---
+++ vue_2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+import TodoInput from './components/TodoInput.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoList,
+    TodoFooter,
+    TodoInput
+  }
+}
+</script>
+
+<style>
+
+</style>
