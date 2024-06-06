b23dd1c6c3ad23fb9173382fb53692098474c73b
JMG <jmg173@naver.com>
2024-06-06 23:54:21 +0900

    라우터 4에 맞게  수정

--- vue_4/src/router/index.js
+++ vue_4/src/router/index.js
@@ -1,37 +1,18 @@
-// import Vue from 'vue';
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createWebHistory, createRouter } from 'vue-router';
 import NewsView from '../views/NewsView.vue';
 import JobsView from '../views/JobsView.vue';
 import AskView from '../views/AskView.vue';
 
-// Vue.use(VueRouter)
-
-// export const router = new VueRouter({
-//   routes: [
-//     {
-//       // url 주소
-//       path: '/news',
-//       // url 주소로 갔을 때 표시될 컴포넌트
-//       component: NewsView,
-//     },
-//     {
-//       path: '/ask',
-//       component: AskView,
-//     },
-//     {
-//       path: 'jobs',
-//       component: JobsView,
-//     },
-//   ],
-// });
-
 const routes = [
+  { path: '/', redirect: '/news' },
   { path: '/news', component: NewsView },
   { path: '/ask', component: AskView },
   { path: '/jobs', component: JobsView },
 ];
 
-export const router = createRouter({
-  history: createMemoryHistory(),
+const router = createRouter({
+  history: createWebHistory(),
   routes,
 });
+
+export default router;
