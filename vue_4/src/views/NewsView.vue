f16f775857b510ea113318cca06a25d16dbf9d0b
JMG <jmg173@naver.com>
2024-06-09 23:51:45 +0900

    axios 설치

--- vue_4/src/views/NewsView.vue
+++ vue_4/src/views/NewsView.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>new</div>
+  <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    let vm = this;
+    axios
+      .get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function (response) {
+        console.log(response);
+        vm.users = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style scoped></style>
