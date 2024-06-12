2c03f3e217ef3405325b5554dc7ee5abc63caf8c
JMG <jmg173@naver.com>
2024-06-12 23:56:27 +0900

    ask 생성

--- vue_4/src/api/index.js
+++ vue_4/src/api/index.js
@@ -3,9 +3,15 @@ import axios from 'axios';
 const config = {
   baseUrl: 'https://api.hnpwa.com/v0/',
 };
+
 function fetchNewsList() {
   // return axios.get(config.baseUrl + '/news/1.json');
   return axios.get(`${config.baseUrl}news/1.json`);
 }
 
-export { fetchNewsList };
+function fetchAskList() {
+  // return axios.get(config.baseUrl + '/news/1.json');
+  return axios.get(`${config.baseUrl}ask/1.json`);
+}
+
+export { fetchNewsList, fetchAskList };
